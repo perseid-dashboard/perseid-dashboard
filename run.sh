efeb4325911e8a0ddb1e0291359ddb16346bb47d
Atanas Hristov <atanashristov@hotmail.com>
2015-08-24 10:26:56 -0500

    bash scripot changes ...

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-export ROOT_URL=http://45.55.207.159:8080/
+## export ROOT_URL=http://45.55.207.159:8080/
 export MONGO_URL=mongodb://sa:test@ds035613.mongolab.com:35613/perseid
-meteor
+meteor -- port 80
 
 exit 1;
