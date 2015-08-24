004c1f3417beb8ee71aec02b78192941ec437241
Atanas Hristov <atanashristov@hotmail.com>
2015-08-24 10:30:14 -0500

    bash script changes ...

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
 
 ## export ROOT_URL=http://45.55.207.159:8080/
-export MONGO_URL=mongodb://sa:test@ds035613.mongolab.com:35613/perseid
-meteor -- port 80
-
-exit 1;
+MONGO_URL=mongodb://sa:test@ds035613.mongolab.com:35613/perseid meteor
